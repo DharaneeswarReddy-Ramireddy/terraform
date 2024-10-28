058ecad9a93492dec995cf53d02e560f1e10ee45
dharan <dharaneeswar.ramireddy@gmail.com>
2024-10-28 16:37:14 +0000

    latest

--- terraform_project/providers.tf
+++ terraform_project/providers.tf
@@ -1,6 +1,24 @@
 # providers.tf
 terraform {
 
+  required_version = ">= 1.0.0"
+
+
+
+  required_providers {
+
+    aws = {
+
+      source  = "hashicorp/aws"
+
+      version = "= 5.73.0"  # Use the exact version from the lock file
+
+    }
+
+  }
+
+
+
   backend "s3" {
 
     bucket         = "dharan-s3-tf"
